71c67b5578f1f8071b7bb9d241213537933808e6
walkerchang <moonwalkers34@gmail.com>
2019-09-10 10:10:51 +0000

    Add 'xfs' type storage class

--- roles/storage-class/files/change-monitors.sh
+++ roles/storage-class/files/change-monitors.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 FILE_PATH=/home/ubuntu
 CEPH_FILE=$FILE_PATH/ceph.conf
-TARGET_FILE=class.yaml
+SC_EXT4_FILE=class-ext4.yaml
+SC_XFS_FILE=class-xfs.yaml
 
 ## get monitor ips
 MONITOR=`grep -n "mon_host" $CEPH_FILE | awk -F'=' '{print $2}' | sed 's/ //g'`
 MONITOR_ADD_PORT=`echo $MONITOR | sed -r 's/,/:6789,/g'`
 
 ## replace monitors
-sed -i "s/YOUR_MONITOR_IP/$MONITOR_ADD_PORT/g" $TARGET_FILE
-
+sed -i "s/YOUR_MONITOR_IP/$MONITOR_ADD_PORT/g" $SC_EXT4_FILE
+sed -i "s/YOUR_MONITOR_IP/$MONITOR_ADD_PORT/g" $SC_XFS_FILE
